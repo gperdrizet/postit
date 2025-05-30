c852313a7b8cccea5753aadbc962e8216121f0d4
George Perdrizet <george@perdrizet.org>
2025-05-30 01:37:41 -0400

    Updated redis container image.

--- redis/start_server.sh
+++ redis/start_server.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-# Set memory overcommit
-sysctl vm.overcommit_memory=1
-
 # Start redis server
-redis-server \
---bind $REDIS_IP \
---requirepass $REDIS_PASSWORD
+redis-server --protected-mode yes --bind $REDIS_IP --requirepass $REDIS_PASSWORD
